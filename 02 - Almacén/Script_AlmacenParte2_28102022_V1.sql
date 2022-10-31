d43875a15f615cfa7d232d28519a6902f70cbdb7
oamaya7 <108418382+oamaya7@users.noreply.github.com>
2022-10-31 11:25:34 -0500

    Update Script_AlmacenParte2_28102022_V1.sql

--- 02 - Almacén/Script_AlmacenParte2_28102022_V1.sql
+++ 02 - Almacén/Script_AlmacenParte2_28102022_V1.sql
@@ -3,35 +3,8 @@
 --****************************************************************
 
 --****************************************************************
--- CREACIÓN DE TABLAS.
+-- MODIFICACIÓN DE TABLAS.
 --****************************************************************
-
-CREATE TABLE public."T057Articulos" (
-    "T057idArticulo" integer NOT NULL,
-    "T057consecItemArticulo" integer,
-    "T057codigoArticulo" integer NOT NULL,
-    "T057Cod_TipoArticulo" smallint NOT NULL,
-    "T057nombre" character varying(50) NOT NULL,
-    "T057descripcion" character varying(255) NOT NULL,
-    "T057docIdentidad" character varying(50),
-    "T057Id_UnidadMedida" smallint NOT NULL,
-    "T057Id_PorcentajeIVA" smallint NOT NULL,
-    "T057Cod_MetodoValoracion" character(5) NOT NULL,
-    "T057nombreCientifico" character varying(100),
-    "T057Id_Marca" smallint,
-    "T057itemDeMovimiento" boolean NOT NULL,
-    "T057stockMinimo" integer,
-    "T057stockMaximo" integer,
-    "T057solicitablePorVivero" boolean NOT NULL,
-    "T057Cod_TipoDepreciacion" character(5) NOT NULL,
-    "T057vidaUtil" integer NOT NULL,
-    "T057Cod_UnidadMedidaVidaUtil" character(5) NOT NULL,
-    "T057valorResidual" integer,
-    "T057tieneHojaDeVida" boolean NOT NULL,
-    "T057Cod_TipoHojaDeVida" character(5)
-);
-
-ALTER TABLE public."T057Articulos" OWNER TO postgres;
 
 
 ALTER TABLE IF EXISTS public."T005EstadoCivil"
@@ -40,7 +13,7 @@ ALTER TABLE IF EXISTS public."T005EstadoCivil"
 
 ALTER TABLE IF EXISTS public."T006TiposDocumentoID"
     ADD COLUMN "T006activo" boolean NOT NULL DEFAULT TRUE,
-    ADD COLUMN "T006itemYaUsado" boolean NOT NULL FALSE;
+    ADD COLUMN "T006itemYaUsado" boolean NOT NULL DEFAULT FALSE;
 
 ALTER TABLE IF EXISTS public."T052Marcas"
     ADD COLUMN "T052activo" boolean NOT NULL DEFAULT TRUE,
