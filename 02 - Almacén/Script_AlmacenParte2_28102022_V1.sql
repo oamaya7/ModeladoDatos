f1df2bf918797ca051362eb0815f4b68e7f1e8f5
oamaya7 <108418382+oamaya7@users.noreply.github.com>
2022-11-02 16:23:59 -0500

    Módulos y Permisos Organigramas

--- 02 - Almacén/Script_AlmacenParte2_28102022_V1.sql
+++ 02 - Almacén/Script_AlmacenParte2_28102022_V1.sql
@@ -30,3 +30,26 @@ ALTER TABLE IF EXISTS public."T055UnidadesMedida"
 ALTER TABLE IF EXISTS public."T056Bodegas"
     ADD COLUMN "T056activo" boolean NOT NULL DEFAULT TRUE,
     ADD COLUMN "T056itemYaUsado" boolean NOT NULL DEFAULT FALSE;
+
+
+-- MODULOS
+-- Módulo para administrar los ORGANIGRAMAS del sistema, implica organigrama, sus niveles y sus unidades organizacionales.
+INSERT INTO public."TzModulos" ("TzIdModulo", "Tznombre", "Tzdescripcion", "Tzsubsistema")
+OVERRIDING SYSTEM VALUE
+VALUES (15, 'Organigramas', 'Permite administrar los organigramas del sistema','TRSV');
+
+-- Módulo para cambiar de ORGANIGRAMA vigente de la entidad en el sistema.
+INSERT INTO public."TzModulos" ("TzIdModulo", "Tznombre", "Tzdescripcion", "Tzsubsistema")
+OVERRIDING SYSTEM VALUE
+VALUES (16, 'Cambio de Organigrama Actual', 'Permite adoptar un nuevo organigrama de la entidad en el sistema','TRSV');
+
+
+-- PERMISOS POR MODULO
+-- Módulo ORGANIGRAMAS
+INSERT INTO public."TzPermisos_Modulo" ("TzIdPermisos_Modulo", "TzId_Modulo", "TzCod_Permiso") OVERRIDING SYSTEM VALUE VALUES (41, 15, 'CR');
+INSERT INTO public."TzPermisos_Modulo" ("TzIdPermisos_Modulo", "TzId_Modulo", "TzCod_Permiso") OVERRIDING SYSTEM VALUE VALUES (42, 15, 'AC');
+INSERT INTO public."TzPermisos_Modulo" ("TzIdPermisos_Modulo", "TzId_Modulo", "TzCod_Permiso") OVERRIDING SYSTEM VALUE VALUES (43, 15, 'CO');
+
+-- Módulo CAMBIO DE ORGANIGRAMA
+INSERT INTO public."TzPermisos_Modulo" ("TzIdPermisos_Modulo", "TzId_Modulo", "TzCod_Permiso") OVERRIDING SYSTEM VALUE VALUES (44, 16, 'EJ');
+INSERT INTO public."TzPermisos_Modulo" ("TzIdPermisos_Modulo", "TzId_Modulo", "TzCod_Permiso") OVERRIDING SYSTEM VALUE VALUES (45, 16, 'CO');
