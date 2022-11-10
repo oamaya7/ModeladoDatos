9f35d7f78be561a82872507f5558f6f9f0a030ec
oamaya7 <108418382+oamaya7@users.noreply.github.com>
2022-11-10 15:13:31 -0500

    Nuevo script Gestión Documental

---
+++ 03 - Gestión Documental/3.Script BIA_GestionDoc_10112022_V1.0.sql
@@ -0,0 +1,4 @@
+--****************************************************************
+-- Script de Creación de Base de Datos - Subsistema Gestión Documental - Última Actualizacion 10/11/2022.
+--****************************************************************
+
