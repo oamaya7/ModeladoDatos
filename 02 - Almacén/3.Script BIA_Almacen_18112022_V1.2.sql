c804934e6a83da4ff31fc02e7bca76bba852853d
oamaya7 <108418382+oamaya7@users.noreply.github.com>
2022-11-23 17:09:24 -0500

    Corrección FK Inventario

--- 02 - Almacén/3.Script BIA_Almacen_18112022_V1.2.sql
+++ 02 - Almacén/3.Script BIA_Almacen_18112022_V1.2.sql
@@ -636,7 +636,7 @@ ALTER TABLE ONLY public."T057CatalogoBienes"
     ADD CONSTRAINT "FK_T057CatalogoBienes_T057Id_BienPadre" FOREIGN KEY ("T057Id_BienPadre") REFERENCES public."T057CatalogoBienes"("T057IdBien");
 
 ALTER TABLE ONLY public."T062Inventario"
-    ADD CONSTRAINT "FK_T062Inventario_T062Id_Articulo" FOREIGN KEY ("T062Id_Articulo") REFERENCES public."T057CatalogoBienes"("T057IdBien");
+    ADD CONSTRAINT "FK_T062Inventario_T062Id_Bien" FOREIGN KEY ("T062Id_Bien") REFERENCES public."T057CatalogoBienes"("T057IdBien");
     
 ALTER TABLE ONLY public."T062Inventario"
     ADD CONSTRAINT "FK_T062Inventario_T062Id_Bodega" FOREIGN KEY ("T062Id_Bodega") REFERENCES public."T056Bodegas"("T056IdBodega");
