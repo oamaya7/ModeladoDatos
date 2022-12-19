3efef3ac20dc4e5a3f914f7a906d4cca5b1efd3e
MiguelISAMC <116747205+MiguelISAMC@users.noreply.github.com>
2022-12-19 15:15:41 -0500

    Vehículos entrega
    Conductor Seguridad
    Almacen tablas vehículos

--- 01 - Seguridad/3.Script BIA_Seguridad_18112022_V1.9.sql
+++ 01 - Seguridad/3.Script BIA_Seguridad_18112022_V1.9.sql
@@ -2128,7 +2128,13 @@ INSERT INTO public."T007ClasesTercero" ("T007IdClaseTercero", "T007nombre")
 OVERRIDING SYSTEM VALUE 
 VALUES (5, 'Aseguradora');
 
+INSERT INTO public."T007ClasesTercero" ("T007IdClaseTercero", "T007nombre")
+OVERRIDING SYSTEM VALUE 
+VALUES (6, 'Conductor');
 
+INSERT INTO public."T007ClasesTercero" ("T007IdClaseTercero", "T007nombre")
+OVERRIDING SYSTEM VALUE 
+VALUES (7, 'Conductor externo');
 
 -- OPERACIONES SOBRE USUARIO
 INSERT INTO public."T008OperacionesSobreUsuario" ("T008CodOperacion", "T008nombre") VALUES ('A', 'Activar');
