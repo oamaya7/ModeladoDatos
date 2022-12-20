bc83a99135e1e5726c60cae1e7bb8523b6d4bbab
oamaya7 <108418382+oamaya7@users.noreply.github.com>
2022-12-20 08:49:52 -0500

    Tabla Viveros Conservación

--- 04 - Conservación/3.Script BIA_Conservacion_19122022_V1.0.sql
+++ 04 - Conservación/3.Script BIA_Conservacion_19122022_V1.0.sql
@@ -27,6 +27,47 @@ ALTER TYPE public."eCodOrigenRecursosVivero" OWNER TO postgres;
  CREACIÓN DE TABLAS.
 ****************************************************************/
 
+CREATE TABLE public."T150Viveros" (
+    "T150IdVivero" smallint GENERATED ALWAYS AS IDENTITY NOT NULL,
+    "T150nombre" character varying(30) NOT NULL,
+    "T150Cod_Municipio" character(5) NOT NULL,
+    "T150direccion" character varying(255) NOT NULL,
+    "T150areaMt2" integer NOT NULL,
+    "T150areaPropagacionMt2" integer NOT NULL,
+    "T150tieneAreaProduccion" boolean NOT NULL,
+    "T150tieneAreaPrepSustrato" boolean NOT NULL,
+    "T150tieneAreaEmbolsado" boolean NOT NULL,
+    "T150codTipoVivero" public."eCodTipoVivero" NOT NULL,
+    "T150Id_ViveristaActual" integer,
+    "T150fechaInicioViveristaActual" timestamp with time zone,
+    "T150codOrigenRecursosVivero" public."eCodOrigenRecursosVivero" NOT NULL,
+    "T150fechaCreacion" timestamp with time zone NOT NULL,
+    "T150Id_PersonaCrea" integer NOT NULL,
+    "T150enFuncionamiento" boolean,
+    "T150fechaUltimaApertura" timestamp with time zone,
+    "T150Id_PersonaAbre" integer,
+    "T150justificacionApertura" character varying(255),
+    "T150fechaCierreActual" timestamp with time zone,
+    "T150Id_PersonaCierra" integer,
+    "T150justificacionCierre" character varying(255),
+    "T150viveroEnCuarentena" boolean,
+    "T150fechaInicioCuarentena" timestamp with time zone,
+    "T150Id_PersonaCuarentena" integer,
+    "T150justificacionCuarentena" character varying(255),
+    "T150rutaArchivoCreacion" character varying(255) NOT NULL,
+    "T150activo" boolean NOT NULL,
+    "T150itemYaUsado" boolean NOT NULL
+
+);
+
+ALTER TABLE public."T150Viveros" OWNER TO postgres;
+
+ALTER TABLE ONLY public."T150Viveros"
+    ADD CONSTRAINT "PK_T150IdVivero" PRIMARY KEY ("T150IdVivero");
+
+ALTER TABLE ONLY public."T150Viveros"
+    ADD CONSTRAINT "T150Viveros_T150nombre_UNQ" UNIQUE ("T150nombre")
+        INCLUDE("T150nombre");
 
 
 
